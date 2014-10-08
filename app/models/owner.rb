8842b3e3fc46d94d6f2c30091bbd1e6b2a4bed84
Hayri <hayri@hoshilab.com>
2014-10-08 16:59:12 +0200

    if deletes owner the cat deletes too

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,4 @@
 class Owner < ActiveRecord::Base
-  has_many :cats
-  accepts_nested_attributes_for :cats,:reject_if => lambda { |c| c[:name].blank? }
+  has_many :cats, dependent: :destroy
+  accepts_nested_attributes_for :cats, allow_destroy: true, :reject_if => lambda { |c| c[:name].blank? }
 end
